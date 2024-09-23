9ad9bfc20ee06cb37e9ca194131eecda0fe8a1e2
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-23 16:19:28 -0500

    Merge code from frontend

--- components/purple_header_component_1724092527.js
+++ components/purple_header_component_1724092527.js
@@ -6,55 +6,7 @@
 
 Vue.component("purple_header_component_1724092527", {
     template: `
-    <header class="w-full z-30" id="header-section-container">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 bg-white">
-            <div class="flex items-center justify-between h-16 md:h-20">
-    
-                <div id="site-branding" class="flex-1"><a id="logo-link" class="inline-flex" href="index.html" aria-label="Cruip"><img id="logo-image" class="max-w-none" src="./images/logo.svg" width="38" height="38" alt="Stellar"></a></div>
-    
-                <nav id="desktop-navigation" class="hidden md:flex md:grow">
-    
-                    <ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center">
-                        <li><a id="Home-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" href="Home.html">Home</a></li>
-                        <li><a id="Browse Sprites-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" href="Browse Sprites.html">Browse Sprites</a></li>
-                        <li><a id="Create-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" href="Create.html">Create</a></li>
-                        <li><a id="Marketplace-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" href="Marketplace.html">Marketplace</a></li>
-                        <li><a id="Community-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" href="Community.html">Community</a></li>
-                    </ul>
-    
-                </nav>
-    
-                <ul id="desktop-sign-in-links" class="flex-1 flex justify-end items-center">
-                    <li><a id="sign-in-link" class="font-medium text-sm whitespace-nowrap transition duration-150 ease-in-out text-slate-300 hover:text-white" href="signin.html">Sign in</a></li>
-                    <li class="ml-6"><a id="sign-up-link" class="btn-sm transition duration-150 ease-in-out w-full group relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none text-slate-300 hover:text-white [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box]" href="signup.html"><span class="relative inline-flex items-center">Sign up <span id="sign-up-arrow" class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1 text-purple-500">-&gt;</span></span></a></li>
-                </ul>
-    
-                <div id="mobile-menu" class="md:hidden flex items-center ml-4" x-data="{ expanded: false }">
-    
-                    <button id="hamburger-button" class="hamburger" :class="{ 'active': expanded }" @click.stop="expanded = !expanded" aria-controls="mobile-nav" :aria-expanded="expanded">
-                        <span class="sr-only">Menu</span>
-                        <svg id="hamburger-icon" class="w-5 h-5 fill-current transition duration-150 ease-in-out text-slate-300 hover:text-white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <rect y="2" width="20" height="2" rx="1" />
-                            <rect y="9" width="20" height="2" rx="1" />
-                            <rect y="16" width="20" height="2" rx="1" />
-                        </svg>
-                    </button>
-    
-                    <nav id="mobile-nav" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" x-ref="mobileNav" :style="expanded ? 'max-height: ' + $refs.mobileNav.scrollHeight + 'px; opacity: 1' : 'max-height: 0; opacity: .8'" @click.outside="expanded = false" @keydown.escape.window="expanded = false" x-cloak>
-                        <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5 [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box]">
-                            <li><a id="mobile-Home-link" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white" href="Home.html">Home</a></li>
-                            <li><a id="mobile-Browse Sprites-link" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white" href="Browse Sprites.html">Browse Sprites</a></li>
-                            <li><a id="mobile-Create-link" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white" href="Create.html">Create</a></li>
-                            <li><a id="mobile-Marketplace-link" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white" href="Marketplace.html">Marketplace</a></li>
-                            <li><a id="mobile-Community-link" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white" href="Community.html">Community</a></li>
-                        </ul>
-                    </nav>
-    
-                </div>
-    
-            </div>
-        </div>
-    </header>`,
+    <header id="header-section-container" class="w-full z-30" data-mwf="true" style="border: 2px solid green;"><div class="max-w-6xl mx-auto px-4 sm:px-6 bg-white"><div class="flex items-center justify-between h-16 md:h-20" style=""><div id="site-branding" class="flex-1"><a id="logo-link" href="index.html" aria-label="Cruip" class="inline-flex"><img id="logo-image" src="./images/logo.svg" width="38" height="38" alt="Stellar" class="max-w-none"></a></div> <nav id="desktop-navigation" class="hidden md:flex md:grow"><ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center"><li><a id="Home-link" href="Home.html" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white">Home</a></li> <li><a id="Browse Sprites-link" href="Browse Sprites.html" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white">Browse Sprites</a></li> <li><a id="Create-link" href="Create.html" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white">Create</a></li> <li><a id="Marketplace-link" href="Marketplace.html" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white">Marketplace</a></li> <li style=""><a id="Community-link" href="Community.html" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-slate-300 hover:text-white" style="" data-mf-new="true">Comm</a></li></ul></nav> <ul id="desktop-sign-in-links" class="flex-1 flex justify-end items-center" style=""><li><a id="sign-in-link" href="signin.html" class="font-medium text-sm whitespace-nowrap transition duration-150 ease-in-out text-slate-300 hover:text-white" style="">Sign in</a></li> <li class="ml-6"><a id="sign-up-link" href="signup.html" class="btn-sm transition duration-150 ease-in-out w-full group relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none text-slate-300 hover:text-white [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box]"><span class="relative inline-flex items-center">Sign up <span id="sign-up-arrow" class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1 text-purple-500">-&gt;</span></span></a></li></ul> <div id="mobile-menu" x-data="{ expanded: false }" class="md:hidden flex items-center ml-4"><button id="hamburger-button" aria-controls="mobile-nav" class="hamburger"><span class="sr-only">Menu</span> <svg id="hamburger-icon" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current transition duration-150 ease-in-out text-slate-300 hover:text-white"><rect y="2" width="20" height="2" rx="1"></rect> <rect y="9" width="20" height="2" rx="1"></rect> <rect y="16" width="20" height="2" rx="1"></rect></svg></button> <nav id="mobile-nav" x-ref="mobileNav" x-cloak="" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" style="max-height: 0px; opacity: 0.8;"><ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5 [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box]"><li><a id="mobile-Home-link" href="Home.html" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white">Home</a></li> <li><a id="mobile-Browse Sprites-link" href="Browse Sprites.html" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white">Browse Sprites</a></li> <li><a id="mobile-Create-link" href="Create.html" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white">Create</a></li> <li><a id="mobile-Marketplace-link" href="Marketplace.html" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white">Marketplace</a></li> <li><a id="mobile-Community-link" href="Community.html" class="flex font-medium text-sm py-1.5 text-slate-300 hover:text-white">Community</a></li></ul></nav></div></div></div></header>`,
         data() {
             return {
                 expanded: false, 
